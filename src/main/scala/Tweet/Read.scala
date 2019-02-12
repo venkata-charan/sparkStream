e9a5e26f6ba63b1a2cdae01313476a6e0f7f30ab
venkata-charan <charanraj.lv@gmail.com>
2019-02-12 06:10:11 +0530

    Adding action to this

--- src/main/scala/Tweet/Read.scala
+++ src/main/scala/Tweet/Read.scala
@@ -31,6 +31,8 @@ object Read extends  App {
   val tweets = TwitterUtils.createStream(ssc,Some(auth))
   val english_tweets = tweets.filter(_.getLang() == "en")
 
+  english_tweets.print(20)
+
   ssc.start()
   ssc.awaitTerminationOrTimeout(30000)
 

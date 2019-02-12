20bc1dcf9d42a67ed3f921fb61c79ef165e98a93
venkata-charan <charanraj.lv@gmail.com>
2019-02-12 12:39:01 +0530

    Adding action to this

--- src/main/scala/Tweet/SaveTweets.scala
+++ src/main/scala/Tweet/SaveTweets.scala
@@ -10,36 +10,13 @@ import twitter4j.conf.ConfigurationBuilder
 object SaveTweets extends App {
 
   println(" --------------------- lets start app -------------------------" )
-  val consumerKey = "vMRUQDIOL13ivVkNeKgLcHe0C"
-  val consumerSecret =  "88TMCrCk08IXESlXs85OqjtTuzBwo2Qv7gIP1IbBhYjLRDEZmh"
-  val accessToken = "1094902540607602688-qWPl4eTnJGKuD4ZFXT01MtohL3Ln2o"
-  val accessTokenSecret = "OM0481sm8qkti91TwYNpzYNuYEuKhlsLRQQfqWDMGBKdh"
 
-  val appName = "cherryTweets"
-  val conf = new SparkConf()
-  conf.setAppName(appName).setMaster("local[2]")
-  val ssc = new StreamingContext(conf,Seconds(5))
-  val sc = ssc.sparkContext
-  sc.setLogLevel("ERROR")
-
-  val cb = new ConfigurationBuilder
-
-  cb.setDebugEnabled(true).setOAuthConsumerKey(consumerKey)
-    .setOAuthConsumerSecret(consumerSecret)
-    .setOAuthAccessToken(accessToken)
-    .setOAuthAccessTokenSecret(accessTokenSecret)
-
-  val auth = new OAuthAuthorization(cb.build())
-
-
-  val tweets = TwitterUtils.createStream(ssc,Some(auth))
- // val english_tweets = tweets.filter(_.getLang() == "en")
-  val statuses = tweets.map(status => status.getText())
-
-   statuses.print(1)
+  val tweets = SetUpTwitter.getTweetSetup("SaveTweets")
+  val hindi_tweets = tweets.filter(_.getLang() == "hi")
+  val statuses = hindi_tweets.map(status => status.getText())
   var TotalTweets:Long = 0
 
-  println(" --------------------- setup done, lets save it ------------------------- and count is " + statuses.count() )
+  println(" --------------------- setup done, lets save it ------------------------- " )
 
   statuses.foreachRDD((rdd,time)=>{
     // don't bother empty Rdd, only consider actual rdd's
@@ -50,7 +27,7 @@ object SaveTweets extends App {
       TotalTweets += repart_Rdd.count()
       println("Tweet count " + TotalTweets)
 
-      if(TotalTweets > 100){
+      if(TotalTweets > 1000){
         System.exit(0)
       }
 

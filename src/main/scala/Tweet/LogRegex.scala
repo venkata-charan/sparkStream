35b864a7d4140e9597d62d6c57801b39b1234bdc
venkata-charan <charanraj.lv@gmail.com>
2019-02-18 15:05:10 +0530

    Log Alarm - to alarm based on apache logs

--- src/main/scala/Tweet/LogRegex.scala
+++ src/main/scala/Tweet/LogRegex.scala
@@ -18,4 +18,21 @@ object LogRegex {
     Pattern.compile(regex)
   }
 
+  def apache2LogPatter():Pattern={
+
+    val ip = "([0-9].*)"
+    val client = "(\\S+)"
+    val user = "(\\S+)"
+    val dateTime = "\\[(.*?)"
+    val nano = "((\\s).*\\])"
+    val request =  "\"(.+?)\""
+    val status = "(\\d{3})"
+    val bytes = "(\\S+)"
+
+    val regex = s"$ip $client $user $dateTime$nano $request $status $bytes"
+    Pattern.compile(regex)
+  }
+
+
+
 }
---
+++ src/main/scala/Tweet/StrucStream.scala
@@ -0,0 +1,58 @@
+package Tweet
+
+import java.util.regex.Matcher
+
+import org.apache.spark.sql.{Row, SparkSession}
+import LogRegex.apache2LogPatter
+import org.apache.spark.sql.types.TimestampType
+import org.apache.spark.sql.functions.unix_timestamp
+import org.apache.spark.streaming.{Minutes, StreamingContext}
+
+
+
+object StrucStream extends App {
+
+  case class LogEntry(ip:String, client:String, user:String, dateTime:String,nano:String, request:String, status:String, bytes:String)
+  val logPattern = apache2LogPatter
+
+  // 1.create spark session
+  // 2.create a stream read on a folder
+  // 3. in each stream session read a line and get status field and time for that record
+  // 4. do count operation on status
+
+  val spark = SparkSession.builder()
+    .appName("Structured Streaming")
+    .config("spark.sql.streaming.checkpointLocation","hdfs:///user/charanrajlv3971/checkpoint/")
+    .getOrCreate()
+
+  val inputStream = spark.readStream.text("hdfs:///user/charanrajlv3971/logs/")
+  import spark.implicits._
+
+  val structuredData = inputStream.flatMap (x => {
+    val matcher:Matcher = logPattern.matcher(x.getString(0))
+    if (matcher.matches()){
+      Some(LogEntry(
+        matcher.group(1),
+        matcher.group(2),
+        matcher.group(3),
+        matcher.group(4),
+        matcher.group(5),
+        matcher.group(6),
+        matcher.group(7),
+        matcher.group(8)))}
+    else None
+  }).toDF().select($"status",
+    unix_timestamp($"dateTime", "dd/MMM/YYYY:HH:mm:ss").cast(TimestampType).as("date")
+  )
+
+  structuredData.createOrReplaceGlobalTempView("Test")
+  val df1 = spark.sql("Select * from Test").show()
+
+  val ssc = new StreamingContext(spark.sparkContext, Minutes(2))
+
+  ssc.start()
+  ssc.awaitTermination()
+
+}
+
+
